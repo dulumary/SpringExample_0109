69e147fed39c007397b872fc7bca4a3e0a66712a
asdf <asdf@gmail.com>
2023-01-13 21:51:37 +0900

    [김인규]  - insert 예제

--- src/main/java/com/marondal/spring/ex/mybatis/ReviewController.java
+++ src/main/java/com/marondal/spring/ex/mybatis/ReviewController.java
@@ -10,18 +10,19 @@ import com.marondal.spring.ex.mybatis.bo.ReviewBO;
 import com.marondal.spring.ex.mybatis.model.Review;
 
 @Controller
+@RequestMapping("/mybatis")
 public class ReviewController {
 	
 	@Autowired
 	private ReviewBO reviewBO;
 	
 	// id가 3인 데이터 조회
-	@RequestMapping("/mybatis/ex01")
+	@RequestMapping("/ex01")
 	@ResponseBody
 	public Review review(
 			@RequestParam("id") int id
 //			@RequestParam(value="id", required=false) int id
-//			@RequestParam(1value="id", defaultValue="3") int id
+//			@RequestParam(value="id", defaultValue="3") int id
 			) {
 		
 		
@@ -29,5 +30,29 @@ public class ReviewController {
 
 		return review;
 	}
+	
+	@RequestMapping("/ex02")
+	@ResponseBody
+	public String addReview() {
+		
+//		int count = reviewBO.addReview(4, "콤비네이션피자", "김인규", 4.5, "할인도 많이 받고 잘 먹었습니다.");
+		
+		Review review = new Review();
+		review.setStoreId(2);
+		review.setMenu("뿌링클");
+		review.setUserName("김인규");
+		review.setPoint(4.0);
+		review.setReview("역시 뿌링클이 진리입니다!!");
+		
+		int count = reviewBO.addReviewByObject(review);
+		
+		return "삽입결과 " + count;
+		
+	}
+	
+	
+	
+	
+	
 
 }
