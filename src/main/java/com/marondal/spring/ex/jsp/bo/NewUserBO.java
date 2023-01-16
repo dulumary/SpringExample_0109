3b91956853285f1773e5a88acc0791b53701ded5
asdf <asdf@gmail.com>
2023-01-16 21:39:32 +0900

    [김인규]  - 기본 jsp model 과정 예제

---
+++ src/main/java/com/marondal/spring/ex/jsp/bo/NewUserBO.java
@@ -0,0 +1,31 @@
+package com.marondal.spring.ex.jsp.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.ex.jsp.dao.NewUserDAO;
+import com.marondal.spring.ex.jsp.model.NewUser;
+
+@Service
+public class NewUserBO {
+	
+	@Autowired
+	private NewUserDAO newUserDAO;
+	
+	public NewUser getLastUser() {
+		
+		return newUserDAO.selectLastUser();
+		
+	}
+	
+	public int addUser(String name
+			, String birthday
+			, String email
+			, String introduce) {
+		
+		return newUserDAO.insertUser(name, birthday, email, introduce);
+	}
+	
+	
+
+}
