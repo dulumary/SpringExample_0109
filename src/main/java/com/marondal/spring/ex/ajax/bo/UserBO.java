7e90f8049dd789af235b02e3da040b84af3290df
asdf <asdf@gmail.com>
2023-01-26 21:50:48 +0900

    [김인규]  - ajax기본 예제

---
+++ src/main/java/com/marondal/spring/ex/ajax/bo/UserBO.java
@@ -0,0 +1,32 @@
+package com.marondal.spring.ex.ajax.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.ex.ajax.dao.UserDAO;
+import com.marondal.spring.ex.jsp.model.NewUser;
+
+@Service
+public class UserBO {
+	
+	@Autowired	
+	private UserDAO userDAO;
+	
+	public List<NewUser> getUserList() {
+		
+		return userDAO.selectUserList();
+		
+	}
+	
+	public int addUser(
+			String name
+			, String yyyymmdd
+			, String email) {
+		
+		return userDAO.insertUser(name, yyyymmdd, email);
+		
+	}
+
+}
